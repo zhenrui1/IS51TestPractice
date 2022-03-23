21efb39b05a7ba70b07a2a592d5c087d947f40a3
zhenrui1 <zhenrui1219@mail.fresnostate.edu>
2022-03-23 12:40:58 -0700

    finish pseudocode

--- test_practice.py
+++ test_practice.py
@@ -14,3 +14,30 @@ if the amount is equal, we output to the user "Option 1 and Option 2 are the sam
 if the option1 is better, we output to the user "Option 1 is better"
 if the option2 is better, we output to the user "Option 2 is better"
 """
+
+"""
+
+# optiion1
+ return 100 * 10
+
+# option2
+  amount = 1
+  list1 = []
+  loop 10 times
+    add amount to list1
+    amount *= 2
+  sum = sum of all items in loop
+  return sum
+# main 
+  var1 = option1
+  var2 = option1
+
+  if var1 = var2
+    "Option 1 and Option 2 are the same"
+  if var1 < var2
+    "Option 2 is better"
+  else
+    "Option 1 is better"
+
+main
+"""
