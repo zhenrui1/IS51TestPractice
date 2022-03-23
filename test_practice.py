e4d4aadecd3162720ff4f5d09ebfac04e18dc362
zhenrui1 <zhenrui1219@mail.fresnostate.edu>
2022-03-23 13:17:37 -0700

    finish exam

--- test_practice.py
+++ test_practice.py
@@ -41,3 +41,31 @@ if the option2 is better, we output to the user "Option 2 is better"
 
 main
 """
+
+def option1(): 
+  return 100 * 10
+
+def option2():
+  amount = 1
+  list1 = []
+  for i in range(0, 10):
+    list1.append(amount)
+    amount *= 2
+  total = sum(list1)
+  return total
+
+
+def main():
+  answer = ""
+  var1 = option1()
+  var2 = option2()
+  if var1 == var2:
+    answer = "Option 1 and Option 2 are the same."
+  elif var1 < var2:
+    answer = "Option 2 is better."
+  else:
+    answer = "Option 1 is better."
+  print(answer)
+
+
+main()
